dfc2076e1e298b97458a74e18c3a665023fe1ede
crocombo <crocombo@gmail.com>
2017-11-23 17:07:42 +0100

    Ajout route des pages depuis fichier routing et controler****************

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,4 +13,37 @@ class DefaultController extends Controller
     {
         return $this->render('AppBundle:Default:index.html.twig');
     }
+
+
+    public function whoareweAction()
+    {
+        return $this->render('AppBundle:Default:whoarewe.html.twig');
+    }
+
+
+     public function newAction()
+    {
+        return $this->render('AppBundle:Default:new.html.twig');
+    }   
+
+
+      public function eventAction()
+    {
+        return $this->render('AppBundle:Default:event.html.twig');
+    }  
+    
+
+     public function partnerAction()
+    {
+        return $this->render('AppBundle:Default:partner.html.twig');
+    }
+
+
+     public function contactAction()
+    {
+        return $this->render('AppBundle:Default:contact.html.twig');
+    }
+
+
+
 }
